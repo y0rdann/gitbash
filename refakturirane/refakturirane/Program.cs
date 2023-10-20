6350c495b3a73ee635eaf762bb6ab74c9b739f73
y0rdann <120708592+y0rdann@users.noreply.github.com>
2023-10-20 11:59:26 +0300

    Add files via upload

---
+++ refakturirane/refakturirane/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refakturirane
+{
+    internal class Program  
+    {
+        public const string FILE_NAME = "example.bin";
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew);
+            BinaryWriter w = new BinaryWriter (fs);
+            for (int i = 0; i<11; i++)
+            {
+                w.Write((int)i);
+            }
+            w.Close();
+            fs.Close();
+            using (fs=new FileStream(FILE_NAME, FileMode.Open, FileAccess.Read));
+            BinaryReader r= new BinaryReader(fs);
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine (r.ReadInt32());
+            }
+            r Close();
+            fs.Close();
+
+        }
+    }
+}
